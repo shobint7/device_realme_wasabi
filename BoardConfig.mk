4040c502c61033c3830a13c8c31cfbe08bf13272
Bruno Martins <bgcngm@gmail.com>
2020-02-19 21:17:32 +0000

    wasabi: Unset BOARD_VNDK_VERSION
    PRODUCT_USE_VNDK will be true in Android-P or later launching devices
    and consequently BOARD_VNDK_VERSION will be set to 'current'.
    
    Change-Id: Iaed3eb4875c00742903627d63b4f2968e356781b

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -157,8 +157,5 @@ BOARD_PLAT_PRIVATE_SEPOLICY_DIR := $(DEVICE_PATH)/sepolicy/private
 # in device system sepolicy to work around IMS issues.
 SELINUX_IGNORE_NEVERALLOWS := true
 
-# Treble
-BOARD_VNDK_VERSION := current
-
 # Inherit from the proprietary version
 -include vendor/realme/wasabi/BoardConfigVendor.mk
