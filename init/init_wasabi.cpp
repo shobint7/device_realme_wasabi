b4bc315596a9e3d7b3bfb7388b44bf4d422bf8ef
TheMalachite <eliasgheeraert@gmail.com>
2020-09-20 04:42:10 +0200

    wasabi: init: Override dalvik props instead
    Change-Id: I04411b154831088ec71ce6b48bc1b887aad3454e

--- init/init_wasabi.cpp
+++ init/init_wasabi.cpp
@@ -30,6 +30,17 @@
 using android::base::GetProperty;
 using android::init::property_set;
 
+void property_override(char const prop[], char const value[])
+{
+	prop_info *pi;
+
+	pi = (prop_info *)__system_property_find(prop);
+	if (pi)
+		__system_property_update(pi, value, strlen(value));
+	else
+		__system_property_add(prop, strlen(prop), value, strlen(value));
+}
+
 char const *heapstartsize;
 char const *heapgrowthlimit;
 char const *heapsize;
@@ -74,10 +85,10 @@ void vendor_load_properties()
 {
     check_device();
 
-    property_set("dalvik.vm.heapstartsize", heapstartsize);
-    property_set("dalvik.vm.heapgrowthlimit", heapgrowthlimit);
-    property_set("dalvik.vm.heapsize", heapsize);
-    property_set("dalvik.vm.heaptargetutilization", heaptargetutilization);
-    property_set("dalvik.vm.heapminfree", heapminfree);
-    property_set("dalvik.vm.heapmaxfree", heapmaxfree);
+    property_override("dalvik.vm.heapstartsize", heapstartsize);
+    property_override("dalvik.vm.heapgrowthlimit", heapgrowthlimit);
+    property_override("dalvik.vm.heapsize", heapsize);
+    property_override("dalvik.vm.heaptargetutilization", heaptargetutilization);
+    property_override("dalvik.vm.heapminfree", heapminfree);
+    property_override("dalvik.vm.heapmaxfree", heapmaxfree);
 }
