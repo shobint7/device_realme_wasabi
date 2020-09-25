5dedec5f8aaa391628a642cef45edd40f662442e
TheMalachite <eliasgheeraert@gmail.com>
2020-09-25 07:37:34 +0200

    wasabi: Build oppo fingerprint blobs
    Change-Id: Ic485723a5c8e2b292bec39d176a0244f0fa5ace9

--- device.mk
+++ device.mk
@@ -59,7 +59,8 @@ PRODUCT_COPY_FILES += \
 
 # Fingerprint
 PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.1-service.wasabi
+    android.hardware.biometrics.fingerprint@2.1-service.wasabi \
+    vendor.oppo.hardware.biometrics.fingerprint@2.1
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.hardware.fingerprint.xml
