3f3f57994d2d3f43f11a882b422143e9f05c5656
TheMalachite <eliasgheeraert@gmail.com>
2020-10-19 18:22:32 +0200

    wasabi: Only import MTK-IMS permissions
    Change-Id: I6a259a99024121647fed9d48c5f83e51190cfd91

--- device.mk
+++ device.mk
@@ -140,6 +140,9 @@ PRODUCT_BOOT_JARS += \
     mediatek-telephony-base \
     mediatek-telephony-common
 
+PRODUCT_COPY_FILES += \
+    $(DEVICE_PATH)/configs/permissions/privapp-permissions-mediatek.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-mediatek.xml
+
 # Trustonic TEE
 PRODUCT_COPY_FILES += \
     $(DEVICE_PATH)/configs/public.libraries-trustonic.txt:$(TARGET_COPY_OUT_SYSTEM)/etc/public.libraries-trustonic.txt
